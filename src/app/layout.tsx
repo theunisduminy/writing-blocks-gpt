a98b33ee666e2b9a711fe9ee2166cd1033a02108
Theunis Duminy <tduminy6@gmail.com>
2025-02-24 15:16:48 +0200

    mobile viewport dynamic view height

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,16 +16,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en' className='max-h-screen overflow-x-hidden'>
+    <html lang='en' className='h-[100dvh] overflow-x-hidden'>
       <body
-        className={`${inter.className} flex min-h-screen flex-col bg-gradient-to-br from-gray-900 from-60% to-prussianBlue`}
+        className={`${inter.className} flex h-[100dvh] flex-col overflow-hidden bg-gradient-to-br from-gray-900 from-60% to-prussianBlue`}
       >
         <header className='bg-gray-900'>
-          <div className='mb-14 flex h-16 items-center border-none md:mb-24'>
+          <div className='mb-8 flex h-16 items-center border-none md:mb-16'>
             <MainNav />
           </div>
         </header>
-        <main className='flex flex-grow items-start justify-center p-4'>
+        <main className='flex flex-1 items-start justify-center overflow-y-auto p-4'>
           {children}
         </main>
         <footer className='bg-transparent p-4 text-center text-sm text-gray-500'>
